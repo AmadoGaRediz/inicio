f33e3bcf3bb33486a7796e98476a3cd8a6636747
AmadoGaRediz <amadogr.ti22@utsjr.edu.mx>
2025-02-05 00:05:41 -0600

    neww

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule], // Importar CommonModule para usar @for
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'paginainicio';
+  proyectos = [
+    { nombre: 'Fundamentos', url: 'https://fundamentosamadogrti22.netlify.app/' },
+    { nombre: 'Ejemplo 2', url: 'https://ejemplo2amadogrti22.netlify.app/' },
+    { nombre: 'Ejemplo 3', url: 'https://ejemplo3amadogrti22.netlify.app/' },
+    { nombre: 'Ejemplo 4', url: 'https://ejemplo4amadogrti22.netlify.app/' },
+    { nombre: 'Ejemplo 5', url: 'https://ejemplo5amadogrti22.netlify.app/' }
+  ];
 }
